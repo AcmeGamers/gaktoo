483cad9dcd3fd2b98571b567d29c3a283cc92be8
acmegamer <stemverse.project@gmail.com>
2022-11-18 21:32:40 +0900

    form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,12 +5,12 @@ function Form () {
         <form>
             <input type="text" placeholder="your name" />
             <label>
-          Pick your favorite flavor:
+          Pick your aimed duration to achieve your goal:
           <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">Grapefruit</option>
-            <option value="lime">Lime</option>
-            <option value="coconut">Coconut</option>
-            <option value="mango">Mango</option>
+            <option value="1month">1-2 Month</option>
+            <option value="3months">3 Months</option>
+            <option value="6months">6 Months</option>
+            <option value="9month">9 Months</option>
           </select>
         </label>
             <button type="submit">Submit</button>
