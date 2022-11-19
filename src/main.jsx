9b3326a5faff8d833b307e0f4d80f75ebae187f4
acmegamer <stemverse.project@gmail.com>
2022-11-19 17:26:14 +0900

    header

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./css/predefined.css";
 import "./css/styles.css";
 import "./css/predefined-query.css";
 import "./css/styles-query.css";
-
+import Header from "./components/Header";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
