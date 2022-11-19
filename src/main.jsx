a35d68bf4c6da013b8d07f01e808c97384bf162d
acmegamer <stemverse.project@gmail.com>
2022-11-19 17:22:25 +0900

    animate

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./css/predefined.css";
 import "./css/styles.css";
 import "./css/predefined-query.css";
 import "./css/styles-query.css";
-
+import "./css/animate.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
--- src/pages/Roadmap/Courses/Roadmap.jsx
+++ src/pages/Roadmap/Courses/Roadmap.jsx
@@ -1,12 +1,13 @@
 import Form from "../../../components/Form";
 import React from "react";
+import "../../../css/animate.css";
 const Roadmap = () => {
   return (
     <div>
-      <div className="roadmap-desc">
+      <div className="roadmap-desc animated shake">
         <h1 class>Roadmap</h1>
 
-        <h2>
+        <h2 className="wow bounce">
           Input just 5 data - get customized roadmap and start talking with
           mentors!
         </h2>
