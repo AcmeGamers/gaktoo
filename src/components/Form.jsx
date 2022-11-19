548a1496572a6f7e65af7324e24a63efdae89379
acmegamer <stemverse.project@gmail.com>
2022-11-19 17:53:16 +0900

    wow

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import "./wow.js";
 function Form() {
   return (
     <div className="form-page">
