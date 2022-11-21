ab57e97be805937e2994184d7799ff9bec3bfadd
Acme Gamers <acmegamers97@hotmail.com>
2022-11-22 02:59:31 +0500

    Update

--- node-vite copy/client/src/components/userPopup.jsx
+++
@@ -1,3 +0,0 @@
-export default function userPopup(props) {
-  // TODO: This function will be called when useEffect checks the user and the user wallet doesn't matches any of the user wallets in the database
-}
